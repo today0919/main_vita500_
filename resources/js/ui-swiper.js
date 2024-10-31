35443d770d99f5988f078fd631e04520b2283877
odd:odd <eehd80yul@gmail.com>
2024-10-31 17:03:47 +0900

    소스정리

--- resources/js/ui-swiper.js
+++ resources/js/ui-swiper.js
@@ -1,39 +1,8 @@
-var swiper = new Swiper(".slide-intro", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  loop: true,
-  // autoplay: true,
-  pagination: {
-    el: ".slide-intro .swiper-pagination",
-    clickable: true,
-  },
-});
-
-var swiper = new Swiper(".slide-studio", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  pagination: {
-    el: ".slide-studio .swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    600: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  },
-});
-
-var swiper = new Swiper(".slide-box3", {
+var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
-  centeredSlides: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
